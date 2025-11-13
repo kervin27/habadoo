2532355b45b912f28baae328b391482770385764
kervin27 <kervin.baquillas@gmail.com>
2025-11-13 17:37:09 +0100

    feat: creazione della home , e navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
+import { Home } from './features/home/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+];
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { Router } from '@angular/router';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MenubarModule, MenuModule, DrawerModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  userMenu: MenuItem[] = [];
+  sidebarVisible = false;
 
+  router = inject(Router);
+
+  ngOnInit() {
+    this.userMenu = [
+      {
+        label: 'Profilo',
+        icon: 'pi pi-user',
+        command: () => this.goToProfile(),
+      },
+      { separator: true },
+      { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout() },
+    ];
+  }
+
+  goToProfile() {
+    console.log('Vai al profilo');
+  }
+
+  logout() {
+    console.log('Logout eseguito');
+    this.sidebarVisible = false; // chiude la sidebar al logout
+  }
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  navigateAndClose(path: string) {
+    this.router.navigate([path]);
+    this.sidebarVisible = false;
+  }
 }
