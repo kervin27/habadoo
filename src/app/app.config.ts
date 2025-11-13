b39761010b6afa279d5f79d8dee91508e0ef839c
kervin27 <kervin.baquillas@gmail.com>
2025-11-13 15:25:18 +0100

    feat: configurazione di firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,35 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { provideFirebaseApp } from '@angular/fire/app';
+import {
+  provideAnalytics,
+  getAnalytics as getAnalyticsFn,
+} from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYec7HjwJe56YtS4l_ehdVOEgZdM8JAN0',
+  authDomain: 'habadoo-app.firebaseapp.com',
+  projectId: 'habadoo-app',
+  storageBucket: 'habadoo-app.firebasestorage.app',
+  messagingSenderId: '713827901334',
+  appId: '1:713827901334:web:d16dc728f0f35bee4747b7',
+  measurementId: 'G-7497D7RZYC',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -20,5 +49,9 @@ export const appConfig: ApplicationConfig = {
         preset: Lara,
       },
     }),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
